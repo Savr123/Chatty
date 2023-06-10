e68e2d40b325adac1288d84f0afb3c2651899cbc
Savr123 <Sangievv@mail.ru>
2023-06-10 17:08:25 +0300

    refactor: -gitignore file was updated - app.js file was refactored, deleted unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,10 @@ import { Route, Routes, BrowserRouter  as Router } from 'react-router-dom';
 
 import Chat from './components/Chat/Chat';
 import NavBar from './components/NavBar';
-import Login from './components/LoginPage';
 
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { Paper } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 
 
 
